0321007f8c16899821c13cef1340c66d3834fab3
lucas <laksteelhouse@gmail.com>
2021-06-06 16:54:51 +0200

    Adds header and sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import './styles/globals.css'
+import './styles/styles.css'
+import Sidebar from './components/Sidebar'
+import Header from './components/Header'
+import BrokerDashboard from './views/BrokerDashboard'
 
 const App = () => {
   return (
     <>
-      <h1>HELLO FLEX COAST PROVIDERS</h1>
+      <Header />
+      <Sidebar />
+      <BrokerDashboard />
     </>
   )
 }
---
+++ src/views/BrokerDashboard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const BrokerDashboard = () => {
+  return <div className='dashboard-container'>HELLO DASHBOARD</div>
+}
+
+export default BrokerDashboard
